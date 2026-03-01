de5962e5f32d3343e8d4f479730e1ad5ea58c49f
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2026-03-01 12:24:33 +0700

    config nativewind

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const WelcomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View className=" w-full h-full flex-column items-center justify-center">
+        <Text className="text-[30px] mt-4">Welcome to RideHub!</Text>
+        {/* <View className="w-full h-[20%] justify-between mt-8 px-5">
+          <TouchableOpacity className="w-full bg-[#1677FF] p-4 rounded-lg">
+            <Link href="/(auth)/login" className="text-white text-center text-lg">
+              Đăng nhập
+            </Link>
+          </TouchableOpacity>
+          <TouchableOpacity className="w-full bg-[#1677FF] p-4 rounded-lg">
+            <Link
+              href={{ pathname: "/signup", params: { step: 1 } }}
+              className="text-white text-center text-lg"
+            >
+              Đăng ký
+            </Link>
+          </TouchableOpacity>
+        </View> */}
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default WelcomeScreen
